3030b545274541da0f707112a9f90c901fba7de0
thillyt <73003317+thillyt@users.noreply.github.com>
2025-03-03 11:58:10 +0000

    update to landing page

---
+++ src/frontend/src/lib/components/homepage/landingPage/landing-page-display.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import IcpLogo from "$lib/icons/ICPLogo.svelte";
+    import LogoIcon from "$lib/icons/LogoIcon.svelte";
+    
+    export let handleLoginClick: () => void;
+</script>
+
+<div class="relative flex flex-col items-center justify-center w-full md:min-h-screen">
+    <div class="items-center w-full max-w-sm mx-4">
+        <div class="flex flex-col items-center justify-center mb-5">
+            <LogoIcon className="mx-auto mb-4 w-24 h-24" />
+            <h2 class="text-2xl text-center cta-text">JOIN THE CLUB</h2>
+        </div>
+        <p class="mb-8 text-center text-BrandGrayShade6">
+            Own the game. Shape the future. Join a global community where fans make the decisions.
+        </p>
+        <div class="flex flex-row items-center justify-center mx-2 mb-4 space-x-2 md:space-x-4">
+            <div class="w-full border border-l border-BrandGrayShade1"></div>
+            <p class="text-center text-xxs whitespace-nowrap">Connect Instantly</p>
+            <div class="w-full border border-r border-BrandGrayShade1"></div>
+        </div>
+        <div class="mx-2 space-y-4">
+            <button 
+                class="flex items-center justify-center w-full py-2 rounded lg:mx-0 bg-BrandGrayShade1 hover:bg-BrandGrayShade3"
+                on:click={handleLoginClick}
+            >
+                <span class="flex items-center justify-center mr-2"><IcpLogo/></span> 
+                <span>Internet Identity</span>
+            </button>
+            <div class="mt-6 text-xs text-center">
+                <p class="mt-6 text-sm text-center text-white">
+                    Have questions? Read our Whitepaper
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -2,10 +2,9 @@
   import { onMount } from "svelte";
   import Layout from "./Layout.svelte";
   import { appStore } from "$lib/stores/app-store";
-  import LogoIcon from "$lib/icons/LogoIcon.svelte";
   import { authStore, type AuthSignInParams } from "$lib/stores/auth-store";
-    import IcpLogo from "$lib/icons/ICPLogo.svelte";
-    import { userStore } from "$lib/stores/user-store";
+  import { userStore } from "$lib/stores/user-store";
+  import LandingPage from "$lib/components/homepage/landingPage/landing-page.svelte";
 
   let isLoggedIn: Boolean;
     let termsAgreed: Boolean;
@@ -22,16 +21,6 @@
       isLoading = false;
     }
   });
-
-
-
-  async function handleLogin() {
-    let params: AuthSignInParams = {
-        domain: import.meta.env.VITE_AUTH_PROVIDER_URL,
-    };
-    await authStore.signIn(params);
-    checkUser();
-  }
 
   async function checkUser(){
       authStore.subscribe((store) => {
@@ -53,35 +42,7 @@
 
 <Layout>
   {#if !isLoggedIn}
-    <div class="flex flex-col md:flex-row min-h-screen w-full bg-BrandGray">
-      <div class="w-full md:w-1/2 bg-cover bg-center" style="background-image: url('background.jpg')">
-        <div class="h-full flex items-end p-4 bg-gradient-to-t from-black/70 to-transparent">
-          <h1 class="text-white text-2xl font-bold text-center py-32 md:py-0">
-            Be Part of the World's First Fully Decentralised Football Club
-          </h1>
-        </div>
-      </div>
-    
-      <div class="w-full md:w-1/2 flex flex-col items-center justify-center p-4 bg-BrandGray text-white">
-        <div class="w-full max-w-sm">
-          <LogoIcon className="mx-auto mb-4 w-24 h-24" />
-          <h2 class="text-2xl cta-text text-center mb-4">JOIN THE CLUB</h2>
-          <p class="text-center mb-6 text-BrandGrayShade6">
-            Own the game. Shape the future. Join a global community where fans make the decisions.
-          </p>
-          <div class="space-y-4">
-            <button 
-              on:click={handleLogin}          
-              class="w-full bg-BrandGrayShade1 hover:bg-BrandGrayShade3 text-white py-2 rounded flex items-center justify-center">
-              <span class="mr-2"><IcpLogo /></span> Internet Identity
-            </button>
-          </div>
-          <p class="text-center mt-6 text-white text-sm">
-            Have questions? Read our Whitepaper
-          </p>
-        </div>
-      </div>
-    </div>
+    <LandingPage />
   {:else}
     {#if user}
       <p>Logged in with principal ID {user.principalId}</p>
