48aef53dba6b5d2963ec6e817a7cad01e564083c
jamesbeadle <jamesbeadle1989@gmail.com>
2025-03-09 20:17:22 +0000

    Update whitepaper

--- src/frontend/src/lib/components/whitepaper/notes.svelte
+++ src/frontend/src/lib/components/whitepaper/notes.svelte
@@ -12,17 +12,11 @@
     </p>
     
     <p>
-        The initial whitepaper can be found at <a href="https://openfpl.xyz/whitepaper" target="_blank">https://openfpl.xyz/whitepaper</a>.
-    </p>
-    
-    <p>
-        Our vision has rapidly expanded into a broader ecosystem encompassing women’s fantasy football, community-driven governance, and a unique betting platform.
+        The OpenFPL DAO has grown leading to the ICFC rebranding. Our vision has expanded into a broader ecosystem encompassing women’s fantasy football, community-driven governance and a unique betting platform. The initial whitepaper can be found at <a class="text-BrandInfo" href="https://openfpl.xyz/whitepaper" target="_blank">https://openfpl.xyz/whitepaper</a>.
     </p>
 
-    <p>ICFC is a rebranding built on the initial 25% decentralisation sale of OpenFPL. </p>
+    <p>Full details of the first SNS decentralisation sale can be found <a class="text-BrandInfo" href="https://nns.ic0.app/project/?project=gyito-zyaaa-aaaaq-aacpq-cai" target="_blank">here</a>.</p>
 
-    <p>Full details of the first SNS decentralisation sale can be found <a href="https://nns.ic0.app/project/?project=gyito-zyaaa-aaaaq-aacpq-cai" target="_blank">here</a>.</p>
-
-    <p>Full IC dashboard information on the DAO can be found <a href="https://dashboard.internetcomputer.org/sns/gyito-zyaaa-aaaaq-aacpq-cai" target="_blank">here</a>.</p>
+    <p>Full IC dashboard information on the DAO can be found <a class="text-BrandInfo" href="https://dashboard.internetcomputer.org/sns/gyito-zyaaa-aaaaq-aacpq-cai" target="_blank">here</a>.</p>
 
 </WhitepaperPage>
--- src/frontend/src/lib/components/whitepaper/sale.svelte
+++ src/frontend/src/lib/components/whitepaper/sale.svelte
@@ -3,80 +3,55 @@
     import HeaderContentSection from "../shared/header-content-section.svelte";
     import ListSection from "./list-section.svelte";
     import WhitepaperPage from "./whitepaper-page.svelte";
+    import WhitepaperSection from "./whitepaper-section.svelte";
 
 </script>
 <WhitepaperPage
     title="Decentralisation Sale 2: Financial Focus"
     intro="A swap will occur throughout April 2025, if successful, decentralising 25% of the ICFC platform.">
-
-
-    <p>
-        ICFC operates as a Decentralised Autonomous Organisation (DAO) with a structured financial model:
-    </p>
-
-    <p>
-        The DAO will hold cryptocurrency balance of Bitcoin (ckBTC), ICP and ICFC tokens along with any user ICRC token assets deposited into our ecosystem for our range of football based applications.
-    </p>
-
-    <p>
-        Waterway Labs provides foundational support to the ICFC ecosystem, controlling around 5% of the token supply through their developer neuron. Their main focus is to add value to the ICFC ecosystem, targeting the distribution of profits to ICFC staked neuron holders.
-    </p>
-
-    <p>
-        Along with ensuring the delivery of software, Waterway Labs also ensures execution of strategic growth initiatives.
-    </p>
-
-    <p>
-        Waterway Labs will legally own any football clubs acquired and will be responsible for generating profits and reinvesting back into club development.
-    </p>
     
-    <ListSection
-        title="Token Sale"
-        items={[
-            "Total Tokens for Sale: 25 million ICFC (formerly FPL).",
-            "Swap Currency: ckBTC.",
-            "Minimum amount for swap to be successful: 50 ckBTC.",
-            "Maximum amount to be raised: 100 ckBTC."
-        ]}
-    />
-    
-    <ListSection
-        title="Purpose of Funds"
-        items={[
-            "Acquiring real-world football clubs.",
-            "Developing and refining icfc.app.",
-            "Seeding liquidity pools for future token trading."
-        ]}
-    />
 
-    <p class="sub-header">
-        Token Lockup & Vesting 
-    </p>
+    <WhitepaperSection title="Token Sale">
+        <p>25 million ICFC will be transferred from the ICFC SNS treasury into a swap canister to undertake a fully automated sale.</p>
+        <ListSection
+            title="ICFC Swap Details"
+            items={[
+                "ICFC to be swapped: 25 million.",
+                "Swap Currency: ckBTC.",
+                "Minimum amount for swap to be successful: 50 ckBTC.",
+                "Maximum amount to be raised: 100 ckBTC."
+            ]}
+        />
+        
+        <ListSection
+            title="Purpose of Funds"
+            items={[
+                "Acquiring a real-world football club.",
+                "Continued development & promotion of the ICFC ecosystem.",
+                "Providing liquidity for future token trading."
+            ]}
+        />     
+    </WhitepaperSection>
+
+    <WhitepaperSection title="Token Vesting Schedule:">
+        <HeaderContentSection title='Dissolve Delays' content="Tokens are securely locked in five non-dissolving neurons with delays equally staggered ranging from 6 months to a 2.5-year delay." />
+        <HeaderContentSection title='Liquid Tokens' content="When fully dissolved, liquid ICFC tokens are added to your ICFC wallet balance. Participants can log into the ICFC platform to track the dissolution of their ICFC neurons, confirming the amount that becomes liquid." />
+    </WhitepaperSection>
 
     <p>
-        Tokens are securely locked in five non-dissolving neurons with a staggered release schedule ranging from six months to a 2.5-year delay. 
+        After the sale the DAO will remain in control of the Bitcoin (ckBTC) raised, only withdrawable via a DAO propoasl.
     </p>
-    <p>
-        When fully dissolved, liquid ICFC tokens are added to your ICFC wallet balance. Participants can log into the ICFC platform to track the dissolution of their ICFC neurons, confirming the amount that becomes liquid.
-    </p>
-
-
 
     <p class="small-header">
         RWA Acquisition 
     </p>
 
     <p>
-        The largest use of funds will be to purchase a UK football club through a broker here in the UK to run through the ICFC DAO.
+        The largest use of the funds raised will be to purchase a UK football club through a broker here in the UK to run through the ICFC DAO. Our aim is to bring the football club under complete control of the DAO, we anticipate future legal frameworks that will make this possible.
     </p>
 
     <p>
-        For clubs acquired by ICFC, their revenue streams will remain fully with each club. 
-        If additional financial support is needed, ICFC will aim to provide direct cash injections into the clubs rather than reallocating existing revenue.
-    </p>
-    <p>
-        This approach ensures that each club retains financial independence and continues to reinvest in its own growth and success, 
-        while still benefiting from the strategic support provided by ICFC.
+        For clubs acquired by ICFC, their revenue streams will remain fully with each club. This approach ensures that each club retains financial independence and continues to reinvest in its own growth and success, while still benefiting from the strategic support provided by ICFC.
     </p>
 
 </WhitepaperPage>
