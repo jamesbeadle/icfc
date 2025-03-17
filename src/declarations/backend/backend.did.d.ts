b61fad5a0e7c9dd88323fe88dc96b824133c91cd
JohnNixon6972 <johnbrightnixon@gmail.com>
2025-03-17 14:48:48 +0000

    revert declarations and .env

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -1,129 +1,62 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-import type { IDL } from '@dfinity/candid';
+import type { Principal } from "@dfinity/principal";
+import type { ActorMethod } from "@dfinity/agent";
+import type { IDL } from "@dfinity/candid";
 
-export interface Account {
-  'owner' : [] | [Principal],
-  'subaccount' : [] | [Subaccount],
+export interface AppStatusDTO {
+  version: string;
+  onHold: boolean;
 }
-export interface AppStatusDTO { 'version' : string, 'onHold' : boolean }
 export interface CreateProfile {
-  'username' : string,
-  'displayName' : string,
-  'profilePictureExtension' : [] | [string],
-  'profilePicture' : [] | [Uint8Array | number[]],
+  username: string;
+  displayName: string;
+  profilePictureExtension: [] | [string];
+  profilePicture: [] | [Uint8Array | number[]];
 }
-export interface DisburseMaturityInProgress {
-  'timestamp_of_disbursement_seconds' : bigint,
-  'amount_e8s' : bigint,
-  'account_to_disburse_to' : [] | [Account],
-  'finalize_disbursement_timestamp_seconds' : [] | [bigint],
-}
-export type DissolveState = { 'DissolveDelaySeconds' : bigint } |
-  { 'WhenDissolvedTimestampSeconds' : bigint };
-export type Error = { 'InvalidProfilePicture' : null } |
-  { 'DecodeError' : null } |
-  { 'TooLong' : null } |
-  { 'NotAllowed' : null } |
-  { 'NotEnoughFunds' : null } |
-  { 'TooShort' : null } |
-  { 'NotFound' : null } |
-  { 'AlreadyClaimed' : null } |
-  { 'NotAuthorized' : null } |
-  { 'InvalidData' : null } |
-  { 'AlreadyExists' : null } |
-  { 'CreateGameError' : null } |
-  { 'UpdateFailed' : null } |
-  { 'OutOfRange' : null } |
-  { 'PaymentError' : null } |
-  { 'CanisterFull' : null } |
-  { 'InEligible' : null };
-export interface Followees { 'followees' : Array<NeuronId> }
-export interface MembershipClaim {
-  'expiresOn' : [] | [bigint],
-  'claimedOn' : bigint,
-  'membershipType' : MembershipType,
-}
-export type MembershipType = { 'NotClaimed' : null } |
-  { 'Seasonal' : null } |
-  { 'Lifetime' : null } |
-  { 'Monthly' : null } |
-  { 'Expired' : null };
-export interface Neuron {
-  'id' : [] | [NeuronId],
-  'staked_maturity_e8s_equivalent' : [] | [bigint],
-  'permissions' : Array<NeuronPermission>,
-  'maturity_e8s_equivalent' : bigint,
-  'cached_neuron_stake_e8s' : bigint,
-  'created_timestamp_seconds' : bigint,
-  'source_nns_neuron_id' : [] | [bigint],
-  'auto_stake_maturity' : [] | [boolean],
-  'aging_since_timestamp_seconds' : bigint,
-  'dissolve_state' : [] | [DissolveState],
-  'voting_power_percentage_multiplier' : bigint,
-  'vesting_period_seconds' : [] | [bigint],
-  'disburse_maturity_in_progress' : Array<DisburseMaturityInProgress>,
-  'followees' : Array<[bigint, Followees]>,
-  'neuron_fees_e8s' : bigint,
-}
-export interface NeuronId { 'id' : Uint8Array | number[] }
-export interface NeuronPermission {
-  'principal' : [] | [Principal],
-  'permission_type' : Int32Array | number[],
-}
+export type Error =
+  | { InvalidProfilePicture: null }
+  | { DecodeError: null }
+  | { TooLong: null }
+  | { NotAllowed: null }
+  | { NotEnoughFunds: null }
+  | { TooShort: null }
+  | { NotFound: null }
+  | { NotAuthorized: null }
+  | { InvalidData: null }
+  | { AlreadyExists: null }
+  | { CreateGameError: null }
+  | { OutOfRange: null }
+  | { PaymentError: null }
+  | { CanisterFull: null };
 export type PrincipalId = string;
 export interface Profile {
-  'username' : string,
-  'displayName' : string,
-  'createdOn' : bigint,
-  'podcastIds' : Array<PrincipalId>,
-  'profilePictureExtension' : string,
-  'membershipClaims' : Array<MembershipClaim>,
-  'appPrincipalIds' : Array<[SubApp, PrincipalId]>,
-  'profilePicture' : [] | [Uint8Array | number[]],
-  'membershipType' : MembershipType,
-  'termsAgreed' : boolean,
-  'membershipExpiryTime' : bigint,
-  'principalId' : PrincipalId,
+  username: string;
+  profilePictureExtension: string;
+  profilePicture: [] | [Uint8Array | number[]];
+  principalId: PrincipalId;
 }
-export type Result = { 'ok' : null } |
-  { 'err' : Error };
-export type Result_1 = { 'ok' : AppStatusDTO } |
-  { 'err' : Error };
-export type Result_2 = { 'ok' : Array<Neuron> } |
-  { 'err' : Error };
-export type Result_3 = { 'ok' : Profile } |
-  { 'err' : Error };
-export type Result_4 = { 'ok' : MembershipClaim } |
-  { 'err' : Error };
+export type Result = { ok: null } | { err: Error };
+export type Result_1 = { ok: AppStatusDTO } | { err: Error };
+export type Result_2 = { ok: Profile } | { err: Error };
 export interface Self {
-  'claimMembership' : ActorMethod<[], Result_4>,
-  'createProfile' : ActorMethod<[CreateProfile], Result>,
-  'getProfile' : ActorMethod<[], Result_3>,
-  'getUserNeurons' : ActorMethod<[], Result_2>,
-  'get_app_status' : ActorMethod<[], Result_1>,
-  'updateDisplayName' : ActorMethod<[UpdateDisplayName], Result>,
-  'updateProfilePicture' : ActorMethod<[UpdateProfilePicture], Result>,
-  'updateUsername' : ActorMethod<[UpdateUserName], Result>,
+  createProfile: ActorMethod<[CreateProfile], Result>;
+  getProfile: ActorMethod<[], Result_2>;
+  get_app_status: ActorMethod<[], Result_1>;
+  updateDisplayName: ActorMethod<[UpdateDisplayName], Result>;
+  updateProfilePicture: ActorMethod<[UpdateProfilePicture], Result>;
+  updateUsername: ActorMethod<[UpdateUserName], Result>;
 }
-export type SubApp = { 'OpenFPL' : null } |
-  { 'OpenWSL' : null } |
-  { 'FootballGod' : null } |
-  { 'TransferKings' : null } |
-  { 'JeffBets' : null };
-export interface Subaccount { 'subaccount' : Uint8Array | number[] }
 export interface UpdateDisplayName {
-  'displayName' : string,
-  'principalId' : PrincipalId,
+  displayName: string;
+  principalId: PrincipalId;
 }
 export interface UpdateProfilePicture {
-  'profilePictureExtension' : string,
-  'profilePicture' : [] | [Uint8Array | number[]],
-  'principalId' : PrincipalId,
+  profilePictureExtension: string;
+  profilePicture: [] | [Uint8Array | number[]];
+  principalId: PrincipalId;
 }
 export interface UpdateUserName {
-  'username' : string,
-  'principalId' : PrincipalId,
+  username: string;
+  principalId: PrincipalId;
 }
 export interface _SERVICE extends Self {}
 export declare const idlFactory: IDL.InterfaceFactory;
--- src/declarations/backend/backend.did.js
+++ src/declarations/backend/backend.did.js
@@ -1,133 +1,63 @@
 export const idlFactory = ({ IDL }) => {
-  const MembershipType = IDL.Variant({
-    'NotClaimed' : IDL.Null,
-    'Seasonal' : IDL.Null,
-    'Lifetime' : IDL.Null,
-    'Monthly' : IDL.Null,
-    'Expired' : IDL.Null,
-  });
-  const MembershipClaim = IDL.Record({
-    'expiresOn' : IDL.Opt(IDL.Int),
-    'claimedOn' : IDL.Int,
-    'membershipType' : MembershipType,
+  const CreateProfile = IDL.Record({
+    username: IDL.Text,
+    displayName: IDL.Text,
+    profilePictureExtension: IDL.Opt(IDL.Text),
+    profilePicture: IDL.Opt(IDL.Vec(IDL.Nat8)),
   });
   const Error = IDL.Variant({
-    'InvalidProfilePicture' : IDL.Null,
-    'DecodeError' : IDL.Null,
-    'TooLong' : IDL.Null,
-    'NotAllowed' : IDL.Null,
-    'NotEnoughFunds' : IDL.Null,
-    'TooShort' : IDL.Null,
-    'NotFound' : IDL.Null,
-    'AlreadyClaimed' : IDL.Null,
-    'NotAuthorized' : IDL.Null,
-    'InvalidData' : IDL.Null,
-    'AlreadyExists' : IDL.Null,
-    'CreateGameError' : IDL.Null,
-    'UpdateFailed' : IDL.Null,
-    'OutOfRange' : IDL.Null,
-    'PaymentError' : IDL.Null,
-    'CanisterFull' : IDL.Null,
-    'InEligible' : IDL.Null,
+    InvalidProfilePicture: IDL.Null,
+    DecodeError: IDL.Null,
+    TooLong: IDL.Null,
+    NotAllowed: IDL.Null,
+    NotEnoughFunds: IDL.Null,
+    TooShort: IDL.Null,
+    NotFound: IDL.Null,
+    NotAuthorized: IDL.Null,
+    InvalidData: IDL.Null,
+    AlreadyExists: IDL.Null,
+    CreateGameError: IDL.Null,
+    OutOfRange: IDL.Null,
+    PaymentError: IDL.Null,
+    CanisterFull: IDL.Null,
   });
-  const Result_4 = IDL.Variant({ 'ok' : MembershipClaim, 'err' : Error });
-  const CreateProfile = IDL.Record({
-    'username' : IDL.Text,
-    'displayName' : IDL.Text,
-    'profilePictureExtension' : IDL.Opt(IDL.Text),
-    'profilePicture' : IDL.Opt(IDL.Vec(IDL.Nat8)),
-  });
-  const Result = IDL.Variant({ 'ok' : IDL.Null, 'err' : Error });
+  const Result = IDL.Variant({ ok: IDL.Null, err: Error });
   const PrincipalId = IDL.Text;
-  const SubApp = IDL.Variant({
-    'OpenFPL' : IDL.Null,
-    'OpenWSL' : IDL.Null,
-    'FootballGod' : IDL.Null,
-    'TransferKings' : IDL.Null,
-    'JeffBets' : IDL.Null,
-  });
   const Profile = IDL.Record({
-    'username' : IDL.Text,
-    'displayName' : IDL.Text,
-    'createdOn' : IDL.Int,
-    'podcastIds' : IDL.Vec(PrincipalId),
-    'profilePictureExtension' : IDL.Text,
-    'membershipClaims' : IDL.Vec(MembershipClaim),
-    'appPrincipalIds' : IDL.Vec(IDL.Tuple(SubApp, PrincipalId)),
-    'profilePicture' : IDL.Opt(IDL.Vec(IDL.Nat8)),
-    'membershipType' : MembershipType,
-    'termsAgreed' : IDL.Bool,
-    'membershipExpiryTime' : IDL.Int,
-    'principalId' : PrincipalId,
+    username: IDL.Text,
+    profilePictureExtension: IDL.Text,
+    profilePicture: IDL.Opt(IDL.Vec(IDL.Nat8)),
+    principalId: PrincipalId,
   });
-  const Result_3 = IDL.Variant({ 'ok' : Profile, 'err' : Error });
-  const NeuronId = IDL.Record({ 'id' : IDL.Vec(IDL.Nat8) });
-  const NeuronPermission = IDL.Record({
-    'principal' : IDL.Opt(IDL.Principal),
-    'permission_type' : IDL.Vec(IDL.Int32),
-  });
-  const DissolveState = IDL.Variant({
-    'DissolveDelaySeconds' : IDL.Nat64,
-    'WhenDissolvedTimestampSeconds' : IDL.Nat64,
-  });
-  const Subaccount = IDL.Record({ 'subaccount' : IDL.Vec(IDL.Nat8) });
-  const Account = IDL.Record({
-    'owner' : IDL.Opt(IDL.Principal),
-    'subaccount' : IDL.Opt(Subaccount),
-  });
-  const DisburseMaturityInProgress = IDL.Record({
-    'timestamp_of_disbursement_seconds' : IDL.Nat64,
-    'amount_e8s' : IDL.Nat64,
-    'account_to_disburse_to' : IDL.Opt(Account),
-    'finalize_disbursement_timestamp_seconds' : IDL.Opt(IDL.Nat64),
-  });
-  const Followees = IDL.Record({ 'followees' : IDL.Vec(NeuronId) });
-  const Neuron = IDL.Record({
-    'id' : IDL.Opt(NeuronId),
-    'staked_maturity_e8s_equivalent' : IDL.Opt(IDL.Nat64),
-    'permissions' : IDL.Vec(NeuronPermission),
-    'maturity_e8s_equivalent' : IDL.Nat64,
-    'cached_neuron_stake_e8s' : IDL.Nat64,
-    'created_timestamp_seconds' : IDL.Nat64,
-    'source_nns_neuron_id' : IDL.Opt(IDL.Nat64),
-    'auto_stake_maturity' : IDL.Opt(IDL.Bool),
-    'aging_since_timestamp_seconds' : IDL.Nat64,
-    'dissolve_state' : IDL.Opt(DissolveState),
-    'voting_power_percentage_multiplier' : IDL.Nat64,
-    'vesting_period_seconds' : IDL.Opt(IDL.Nat64),
-    'disburse_maturity_in_progress' : IDL.Vec(DisburseMaturityInProgress),
-    'followees' : IDL.Vec(IDL.Tuple(IDL.Nat64, Followees)),
-    'neuron_fees_e8s' : IDL.Nat64,
-  });
-  const Result_2 = IDL.Variant({ 'ok' : IDL.Vec(Neuron), 'err' : Error });
+  const Result_2 = IDL.Variant({ ok: Profile, err: Error });
   const AppStatusDTO = IDL.Record({
-    'version' : IDL.Text,
-    'onHold' : IDL.Bool,
+    version: IDL.Text,
+    onHold: IDL.Bool,
   });
-  const Result_1 = IDL.Variant({ 'ok' : AppStatusDTO, 'err' : Error });
+  const Result_1 = IDL.Variant({ ok: AppStatusDTO, err: Error });
   const UpdateDisplayName = IDL.Record({
-    'displayName' : IDL.Text,
-    'principalId' : PrincipalId,
+    displayName: IDL.Text,
+    principalId: PrincipalId,
   });
   const UpdateProfilePicture = IDL.Record({
-    'profilePictureExtension' : IDL.Text,
-    'profilePicture' : IDL.Opt(IDL.Vec(IDL.Nat8)),
-    'principalId' : PrincipalId,
+    profilePictureExtension: IDL.Text,
+    profilePicture: IDL.Opt(IDL.Vec(IDL.Nat8)),
+    principalId: PrincipalId,
   });
   const UpdateUserName = IDL.Record({
-    'username' : IDL.Text,
-    'principalId' : PrincipalId,
+    username: IDL.Text,
+    principalId: PrincipalId,
   });
   const Self = IDL.Service({
-    'claimMembership' : IDL.Func([], [Result_4], []),
-    'createProfile' : IDL.Func([CreateProfile], [Result], []),
-    'getProfile' : IDL.Func([], [Result_3], []),
-    'getUserNeurons' : IDL.Func([], [Result_2], []),
-    'get_app_status' : IDL.Func([], [Result_1], ['query']),
-    'updateDisplayName' : IDL.Func([UpdateDisplayName], [Result], []),
-    'updateProfilePicture' : IDL.Func([UpdateProfilePicture], [Result], []),
-    'updateUsername' : IDL.Func([UpdateUserName], [Result], []),
+    createProfile: IDL.Func([CreateProfile], [Result], []),
+    getProfile: IDL.Func([], [Result_2], []),
+    get_app_status: IDL.Func([], [Result_1], ["query"]),
+    updateDisplayName: IDL.Func([UpdateDisplayName], [Result], []),
+    updateProfilePicture: IDL.Func([UpdateProfilePicture], [Result], []),
+    updateUsername: IDL.Func([UpdateUserName], [Result], []),
   });
   return Self;
 };
-export const init = ({ IDL }) => { return []; };
+export const init = ({ IDL }) => {
+  return [];
+};
