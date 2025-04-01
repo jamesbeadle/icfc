51b6cb34659b59fe1293a7e5390a471680353eb3
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-01 20:30:57 +0100

    Update stores

--- src/frontend/src/lib/services/country-service.ts
+++ src/frontend/src/lib/services/country-service.ts
@@ -1,11 +1,12 @@
 import { ActorFactory } from "../utils/ActorFactory";
 import { isError } from "../utils/helpers";
 import { authStore } from "$lib/stores/auth-store";
+import type { Country } from "../../../../declarations/backend/backend.did";
 
 export class CountryService {
   constructor() {}
 
-  async getCountries(): Promise<CountryDTO[]> {
+  async getCountries(): Promise<Country[]> {
     const identityActor: any = await ActorFactory.createIdentityActor(
       authStore,
       process.env.BACKEND_CANISTER_ID ?? "",
