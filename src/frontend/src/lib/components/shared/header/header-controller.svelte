1b3c0583f0aeac5715f404cf9dd907c447858947
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-03 18:32:11 +0100

    Add busy store check

--- src/frontend/src/lib/components/shared/header/header-controller.svelte
+++ src/frontend/src/lib/components/shared/header/header-controller.svelte
@@ -9,6 +9,8 @@
     import LoggedOutHeader from './logged-out-header.svelte';
     import Sidebar from './sidebar.svelte';
     import CreateUser from '$lib/components/profile/create-user/create-user.svelte';
+    import { busy } from '$lib/stores/busy-store';
+    import LocalSpinner from '../local-spinner.svelte';
 
     interface Props {
         children: Snippet;
@@ -22,42 +24,47 @@
     }
 </script>
 
-{#if $authSignedInStore}
-    {#if $userIdCreatedStore?.data}
-        <div class="relative flex flex-col w-full min-h-screen">
-            <LoggedInHeader {toggleMenu} />
-            <main class="flex-1 w-full mt-16 overflow-x-hidden">
-            {@render children()}
-            </main>
-        </div>
-        <Sidebar {isMenuOpen} {toggleMenu} />
-        {#if isMenuOpen}
-            <button 
-            class="fixed inset-0 z-30 pointer-events-none bg-black/40 sm:bg-black/20 sm:pointer-events-auto"
-            onclick={toggleMenu}
-            onkeydown={(e) => e.key === 'Enter' && toggleMenu()}
-            aria-label="Close menu overlay"
-            ></button>
+{#if $busy}
+    <LocalSpinner />
+{:else}
+
+    {#if $authSignedInStore}
+        {#if $userIdCreatedStore?.data}
+            <div class="relative flex flex-col w-full min-h-screen">
+                <LoggedInHeader {toggleMenu} />
+                <main class="flex-1 w-full mt-16 overflow-x-hidden">
+                {@render children()}
+                </main>
+            </div>
+            <Sidebar {isMenuOpen} {toggleMenu} />
+            {#if isMenuOpen}
+                <button 
+                class="fixed inset-0 z-30 pointer-events-none bg-black/40 sm:bg-black/20 sm:pointer-events-auto"
+                onclick={toggleMenu}
+                onkeydown={(e) => e.key === 'Enter' && toggleMenu()}
+                aria-label="Close menu overlay"
+                ></button>
+            {/if}
+        {:else}
+            <div class="relative flex flex-col w-full min-h-screen bg-BrandBlack">
+                <div class="fixed top-0 left-0 right-0 z-50">
+                    <HeaderUserSignup />
+                </div>
+                <div class="mt-12">
+                    <CreateUser />
+                </div>
+            </div>
         {/if}
     {:else}
-        <div class="relative flex flex-col w-full min-h-screen bg-BrandBlack">
-            <div class="fixed top-0 left-0 right-0 z-50">
-                <HeaderUserSignup />
+        {#if page.route.id === '/'}
+            {@render children()}
+        {:else}
+            <div class="relative flex flex-col w-full min-h-screen">
+                <LoggedOutHeader />
+                <main class="flex-1 w-full mt-16 overflow-x-hidden">
+                    {@render children()}
+                </main>
             </div>
-            <div class="mt-12">
-                <CreateUser />
-            </div>
-        </div>
-    {/if}
-{:else}
-    {#if page.route.id === '/'}
-        {@render children()}
-    {:else}
-        <div class="relative flex flex-col w-full min-h-screen">
-            <LoggedOutHeader />
-            <main class="flex-1 w-full mt-16 overflow-x-hidden">
-                {@render children()}
-            </main>
-        </div>
+        {/if}
     {/if}
 {/if}
